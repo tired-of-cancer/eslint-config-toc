cfb4d75aeae87fc81855784426e49afb64b39f85
Rik van Duijn <ik@rikvanduijn.nl>
2022-07-21 13:39:53 +0200

    Add react native specific styles sorting rule

--- react-native.js
+++ react-native.js
@@ -23,5 +23,12 @@ module.exports = {
   ],
 
   // Add React Native specific rule deviations here.
-  rules: {},
+  rules: {
+    // Sort RN styles, this will sort typed stylenames and style props in alphabetic order
+    'react-native/sort-styles': [
+      'error',
+      'asc',
+      { ignoreClassNames: false, ignoreStyleProperties: false },
+    ],
+  },
 };
--- react.js
+++ react.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
 
-  plugins: [],
+  plugins: ['react'],
 
   settings: {
     react: {
